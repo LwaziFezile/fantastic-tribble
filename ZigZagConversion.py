1badf7cddd829aca7af81aa54abaec6ac5422616
lluswazi023 <lluswazi023@student.wethinkcode.co.za>
2024-05-18 14:58:33 +0200

    Added Docstrings

--- ZigZagConversion.py
+++ ZigZagConversion.py
@@ -1,5 +1,19 @@
 class ZigZagConversion :
+    """
+        A class to perform ZigZag Conversion of a given string based on a specified number of rows.
+    """
         def convert(self, s: str, numRows: int) -> str:
+        """
+            Converts the input string into a ZigZag pattern on a given number of rows,
+            and then reads line by line to produce a new string.
+
+            Args:
+                s (str): The input string to be converted.
+                numRows (int): The number of rows to use for the ZigZag pattern.
+
+            Returns:
+                str: The string formed by reading the ZigZag pattern line by line.
+            """
             if numRows == 1:
                 return s
             if numRows % 2 == 0:
